7fc09d24235e6f2a337cb20489e4716b53547277
typeryougishiki <typeryougishiki@163.com>
2024-07-19 14:37:49 +0800

    xxse-csvmanager-header test

---
+++ packages/x/xxse-csvmanager-header/xmake.lua
@@ -0,0 +1,21 @@
+package("xxse-csvmanager-header")
+    set_kind("library", {headeronly = true})
+    set_description("The xxse-csvmanager-header package")
+    set_license("GPL-3.0")
+    add_deps("rapidcsv")
+
+    add_urls("https://github.com/typeryougishiki/xxse-CsvManager-header.git")
+    add_versions("0.1.0", "bfa1f75")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        -- TODO check includes and interfaces
+        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
+    end)
